94ca48578285f4edd4e94f509e4148d03ea607b7
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-03-17 23:40:11 +0900

    Create range() function

---
+++ src/DateSelect/range.test.ts
@@ -0,0 +1,16 @@
+import { assert, describe, expect, it } from 'vitest'
+import { range } from "./range"
+
+describe("range()", () => {
+  const testCases: [number, number, number[]][] = [
+    [1, 1, [1]],
+    [1, 2, [1, 2]],
+    [1, 12, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]],
+    [2, 1, [2, 1]],
+  ]
+  testCases.forEach(([first, last, expected]) => {
+    it(`range(${first}, ${last}) returns ${JSON.stringify(expected)}`, () => {
+      expect(range(first, last)).toEqual(expected)
+    });
+  })
+});
---
+++ src/DateSelect/range.ts
@@ -0,0 +1,11 @@
+export function range(first: number, last: number): number[] {
+  if (first > last) {
+    return range(last, first).reverse()
+  }
+
+  const arr: number[] = [];
+  for (let i = first; i <= last; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
