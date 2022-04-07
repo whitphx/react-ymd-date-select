3f13141c02e96d0726b5230c15e3d6ee1971f554
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-04-08 02:15:16 +0900

    Add a sample using react-hook-form with defaultValues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import VanillaReactHookFormSample from "./samples/vanilla/react-hook-form";
+import VanillaReactHookFormWithDefaultValueSample from "./samples/vanilla/react-hook-form-default-value";
 import MaterialUIReactHookFormSample from "./samples/material/react-hook-form";
 import ChakraUIReactHookFormSample from "./samples/chakra-ui/react-hook-form";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <VanillaReactHookFormSample />
+      <VanillaReactHookFormWithDefaultValueSample />
       <MaterialUIReactHookFormSample />
       <ChakraUIReactHookFormSample />
     </div>
