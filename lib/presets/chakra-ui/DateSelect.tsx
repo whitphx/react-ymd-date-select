8e09b75a80386108d613628706fbab4a8208db00
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-04-17 15:56:45 +0900

    Add hideDay prop to presets/chakra-ui/DateSelect

--- lib/presets/chakra-ui/DateSelect.tsx
+++ lib/presets/chakra-ui/DateSelect.tsx
@@ -1,10 +1,22 @@
 import DateDropdownGroup from "./DateDropdownGroup";
 import Base, { DateSelectProps as BaseProps } from "../../DateSelect";
 
-export type DateSelectProps = Omit<BaseProps, "render">;
+export interface DateSelectProps extends Omit<BaseProps, "render"> {
+  hideDay?: boolean;
+}
 
 function DateSelect(props: DateSelectProps) {
-  return <Base {...props} render={DateDropdownGroup} />;
+  return (
+    <Base
+      {...props}
+      defaultDay={
+        props.defaultDay ? props.defaultDay : props.hideDay ? 1 : undefined
+      }
+      render={(renderProps) => (
+        <DateDropdownGroup {...renderProps} hideDay={props.hideDay} />
+      )}
+    />
+  );
 }
 
 export default DateSelect;
---
+++ samples/chakra-ui/react-hook-form-hide-day.tsx
@@ -0,0 +1,47 @@
+import { useForm, Controller } from "react-hook-form";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+import { DateSelect } from "../../lib/presets/chakra-ui";
+
+type FormData = {
+  date: string;
+};
+
+function ChakraUIReactHookFormHideDaySample() {
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = (data: FormData) => console.log(data);
+
+  console.log(watch("date")); // watch input value by passing the name of it
+
+  const isError = !!errors.date;
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={isError}>
+        <Controller
+          name="date"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <DateSelect {...field} hideDay />}
+        />
+        {!isError ? (
+          <FormHelperText>Enter the date.</FormHelperText>
+        ) : (
+          <FormErrorMessage>This field is required.</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default ChakraUIReactHookFormHideDaySample;
