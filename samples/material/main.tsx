1b6bf27bad0980731eb036cae37985687e5ce662
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-04-24 00:25:51 +0900

    Fix typing

--- samples/material/main.tsx
+++ samples/material/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root");
-const root = createRoot(container);
+const root = createRoot(container!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
 root.render(
   <React.StrictMode>
     <App />
--- site/main.tsx
+++ site/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 
 const container = document.getElementById("root");
-const root = createRoot(container);
+const root = createRoot(container!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
 root.render(
   <React.StrictMode>
     <App />
