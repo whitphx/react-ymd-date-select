c1df8e11aef945d13d8fd88bd4b3fd37a236a747
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-04-24 00:07:58 +0900

    Switch ReactDOM.render to createRoot

--- samples/chakra-ui/main.tsx
+++ samples/chakra-ui/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
