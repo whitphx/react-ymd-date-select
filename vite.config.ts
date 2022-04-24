6e64bbf49195f735227b8a4a9dc339a9f7006bd1
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-04-25 00:07:21 +0900

    Install vite-plugin-mdx and set up it with the minimum config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from "vite-plugin-mdx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mdx({
+      // See https://mdxjs.com/advanced/plugins
+      remarkPlugins: [
+        // E.g. `remark-frontmatter`
+      ],
+      rehypePlugins: [],
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
