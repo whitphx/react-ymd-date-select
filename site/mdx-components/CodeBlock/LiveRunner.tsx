5a3b3190d662d2b3d5db546b21de43e231b19b41
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-09 19:44:05 +0900

    Decorate <LiveRunner /> by copying styles from react-runner's website

--- site/mdx-components/CodeBlock/LiveRunner.tsx
+++ site/mdx-components/CodeBlock/LiveRunner.tsx
@@ -5,6 +5,67 @@ import {
   useLiveRunner,
   UseLiveRunnerProps,
 } from "react-live-runner";
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  box-shadow: 0 0 2px 0 lightsteelblue;
+  height: 300px;
+  overflow: hidden;
+
+  @media (max-width: 640px) {
+    flex-direction: column-reverse;
+    height: 480px;
+  }
+`;
+
+export const EditorContainer = styled.div`
+  flex: 0 1 720px;
+  overflow: auto;
+`;
+
+export const Editor = styled(CodeEditor)`
+  font-family: source-code-pro, Menlo, Monaco, Consolas, Courier New, monospace;
+  font-size: 14px;
+  white-space: pre;
+  caret-color: #fff;
+  min-width: 100%;
+  min-height: 100%;
+  float: left;
+
+  & > textarea,
+  & > pre {
+    outline: none;
+    white-space: pre !important;
+  }
+`;
+
+export const PreviewContainer = styled.div`
+  flex: 1 1 720px;
+  position: relative;
+  display: flex;
+  overflow: hidden;
+`;
+
+export const Preview = styled.div`
+  margin: auto;
+  white-space: pre-wrap;
+  max-width: 100%;
+  max-height: 100%;
+  overflow: auto;
+`;
+
+export const PreviewError = styled.div`
+  background: #fcc;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  margin: 0;
+  padding: 10px;
+  color: #f00;
+  white-space: pre-wrap;
+`;
 
 interface LiveRunnerProps extends UseLiveRunnerProps {
   language: "jsx" | "tsx";
@@ -13,13 +74,15 @@ function LiveRunner({ language, ...liveRunnerProps }: LiveRunnerProps) {
   const { code, element, error, onChange } = useLiveRunner(liveRunnerProps);
 
   return (
-    <div>
-      <div>
-        <CodeEditor value={code} language={language} onChange={onChange} />
-      </div>
-      <div>{element}</div>
-      {error && <p>{error}</p>}
-    </div>
+    <Container>
+      <EditorContainer>
+        <Editor value={code} language={language} onChange={onChange} />
+      </EditorContainer>
+      <PreviewContainer>
+        <Preview>{element}</Preview>
+        {error && <PreviewError>{error}</PreviewError>}
+      </PreviewContainer>
+    </Container>
   );
 }
 
