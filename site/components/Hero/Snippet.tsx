5487038dac0b5426ae103d5db30bb6ad0716d268
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-10 23:50:42 +0900

    demo page style (#52)
    * Update Main.mdx
    
    * Set margin on <CodePreview />
    
    * Add snippets on the hero
    
    * Set style on the show/hide code buttons
    
    * Hide the snippet on small devices
    
    * Add 'See on GitHub' button
    
    * Update the hero layout

---
+++ site/components/Hero/Snippet.tsx
@@ -0,0 +1,47 @@
+import { useCallback, useRef } from "react";
+import styled from "@emotion/styled";
+
+const Code = styled.code`
+  display: inline-block;
+  padding: 8px 16px;
+  border-radius: 5px;
+  border: #888 solid 1px;
+  background: rgba(20, 20, 20, 0.4);
+  color: #eee;
+
+  /* Ref: https://github.com/chakra-ui/chakra-ui/blob/b77acf36688abb696fd1ec2a7898a7de43a91e37/packages/css-reset/src/css-reset.tsx#L43 */
+  font-family: SFMono-Regular, Menlo, Monaco, Consolas, monospace;
+  font-size: 1em;
+`;
+
+interface SnippetProps {
+  children: string;
+}
+function Snippet(props: SnippetProps) {
+  const ref = useRef<HTMLElement | null>(null);
+  const handleClick = useCallback<React.MouseEventHandler>((e) => {
+    if (e.detail !== 2) {
+      return;
+    }
+
+    const elem = ref.current;
+    if (elem == null) {
+      return;
+    }
+
+    // With double click, select all the text.
+    const range = new Range();
+    range.setStart(elem, 0);
+    range.setEnd(elem, 1);
+    document.getSelection()?.removeAllRanges();
+    document.getSelection()?.addRange(range);
+  }, []);
+
+  return (
+    <Code ref={ref} onClick={handleClick}>
+      {props.children}
+    </Code>
+  );
+}
+
+export default Snippet;
--- site/components/Hero/index.tsx
+++ site/components/Hero/index.tsx
@@ -1,23 +1,31 @@
 import styled from "@emotion/styled";
+import { Link } from "@chakra-ui/react";
+import Snippet from "./Snippet";
 
 const HeroContainer = styled.div`
+  position: relative;
   margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   background: #46c9e5; /* fallback color */
   background: linear-gradient(150deg, #46c9e5 0%, #d26ac2 100%);
   height: 300px;
   width: 100%;
-  text-align: center;
 
   @media (max-width: 640px) {
-    height: 150px;
+    height: 200px;
   }
+`;
+
+const TitleContainer = styled.div`
+  text-align: center;
 `;
 
 const Title = styled.h1`
   color: #efefef;
   text-transform: uppercase;
-  display: inline-block;
-  margin-top: 60px;
+  display: block;
   font-size: 3em;
 
   @media (max-width: 640px) {
@@ -25,12 +33,85 @@ const Title = styled.h1`
   }
 `;
 
+const Subtitle = styled.h2`
+  color: #efefef;
+  display: block;
+  font-size: 1em;
+`;
+
+const BodyContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  text-align: center;
+  color: #eee;
+`;
+
+const SnippetBlockContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin: 16px auto 0;
+
+  @media (max-width: 640px) {
+    display: none;
+  }
+`;
+
+const SnippetContainer = styled.p`
+  margin-bottom: 4px;
+`;
+
+const FooterContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+`;
+
+const Footer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 8px 8px;
+  text-align: right;
+`;
+
+const GitHubLink = styled(Link)`
+  display: inline-block;
+  margin-top: 8px;
+  color: #eee;
+`;
+
 function Hero() {
   return (
     <HeroContainer>
-      <div>
+      <TitleContainer>
         <Title>react-ymd-date-select</Title>
-      </div>
+        <Subtitle>
+          Helper hooks and components for Y-M-D dropdowns with React
+        </Subtitle>
+      </TitleContainer>
+
+      <BodyContainer>
+        <SnippetBlockContainer>
+          <SnippetContainer>
+            <Snippet>npm install --save react-ymd-date-select</Snippet>
+          </SnippetContainer>
+          <SnippetContainer>
+            <Snippet>yarn add react-ymd-date-select</Snippet>
+          </SnippetContainer>
+        </SnippetBlockContainer>
+      </BodyContainer>
+
+      <FooterContainer>
+        <Footer>
+          <GitHubLink
+            href="https://github.com/whitphx/react-ymd-date-select"
+            isExternal
+          >
+            See on GitHub
+          </GitHubLink>
+        </Footer>
+      </FooterContainer>
     </HeroContainer>
   );
 }
