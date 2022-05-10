5d057edd16ff46cdfc648e61d350670032ab6681
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-10 13:06:34 +0900

    Remove chakra-ui/react from CodePreview's scope

--- site/components/CodePreview/scope.ts
+++ site/components/CodePreview/scope.ts
@@ -1,5 +1,4 @@
 import React from "react";
-import * as ChakraUiReact from "@chakra-ui/react";
 import * as reactYmdDateSelect from "react-ymd-date-select";
 import * as vanillaPreset from "react-ymd-date-select/presets/vanilla";
 import * as chakraPreset from "react-ymd-date-select/presets/chakra-ui";
@@ -8,7 +7,6 @@ import * as muiPreset from "react-ymd-date-select/presets/material";
 export const scope = {
   import: {
     react: React,
-    "@chakra-ui/react": ChakraUiReact,
     "react-ymd-date-select": reactYmdDateSelect,
     "react-ymd-date-select/presets/vanilla": vanillaPreset,
     "react-ymd-date-select/presets/chakra-ui": chakraPreset,
