b93ec58a7f5b24f98fa8b667338d074379d7ed25
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-11 00:10:58 +0900

    Set title and remove index.css

--- site/main.tsx
+++ site/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import "./index.css";
 import App from "./App";
 
 const theme = extendTheme({
