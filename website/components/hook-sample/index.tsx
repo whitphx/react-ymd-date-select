a76f885f2658a8c612b78dbf850c6e0ed5490cce
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-11 01:22:40 +0900

    Remove unnecessary wrapper website/components/hook-sample/index.tsx

--- website/components/hook-sample/index.tsx
+++
@@ -1,6 +0,0 @@
-import CodePreview from "../CodePreview";
-import SampleCode from "./samples/hook?raw";
-
-export function CustomComponentSample() {
-  return <CodePreview initialCode={SampleCode} language="tsx" />;
-}
