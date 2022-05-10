c56ec23e1d621e24ccd8c327050984757bac5f17
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-10 19:01:38 +0900

    Add react-hook-form sample

---
+++ site/samples/react-hook-form.tsx
@@ -0,0 +1,34 @@
+import { useForm, Controller } from "react-hook-form";
+import { DateSelect } from "react-ymd-date-select/presets/vanilla";
+
+type FormData = {
+  date: string;
+};
+
+function Sample() {
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = (data: FormData) => console.log(data);
+
+  console.log(watch("date")); // watch input value by passing the name of it
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="date"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => <DateSelect {...field} />}
+      />
+      {errors.date && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default Sample;
