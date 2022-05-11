48ff2a9f40adae7834d00ae81c2076518a8d9ced
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-11 17:03:42 +0900

    eyecatch date select invalid state (#64)
    * Add invalid state indicator on <EyeCatchDateSelect />
    
    * Remove blank options from <EyeCatchDateSelect />

--- website/components/EyeCatchDateSelect/index.tsx
+++ website/components/EyeCatchDateSelect/index.tsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
-import { Select } from "@chakra-ui/react";
+import { FormControl, FormErrorMessage, Select } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 import { useDateSelect } from "react-ymd-date-select";
+
+const dropdownIconColor = "#be5f6f";
+const errorColor = "#fcdfff";
 
 const Container = styled.div`
   display: flex;
 
   & > div {
     margin: 0 4px;
-    border-color: rgba(80, 80, 80, 0.4);
+
+    & > select {
+      border-color: rgba(80, 80, 80, 0.4);
+
+      &[aria-invalid="true"] {
+        border-color: ${errorColor};
+        box-shadow: 0 0 0 1px ${errorColor};
+      }
+    }
   }
 `;
 
 function EyeCatchDateSelect() {
   const [date, setDate] = useState("");
+
   const dateSelect = useDateSelect(date, setDate, {
     defaultYear: "now",
     defaultMonth: "now",
@@ -21,47 +33,47 @@ function EyeCatchDateSelect() {
   });
 
   return (
-    <Container>
-      <Select
-        value={dateSelect.yearValue}
-        onChange={dateSelect.onYearChange}
-        bg="whiteAlpha.900"
-        color="#be5f6f"
-      >
-        <option value="" disabled></option>
-        {dateSelect.yearOptions.map((yearOption) => (
-          <option key={yearOption.value} value={yearOption.value}>
-            {yearOption.label}
-          </option>
-        ))}
-      </Select>
-      <Select
-        value={dateSelect.monthValue}
-        onChange={dateSelect.onMonthChange}
-        bg="whiteAlpha.900"
-        color="#be5f6f"
-      >
-        <option value="" disabled></option>
-        {dateSelect.monthOptions.map((monthOption) => (
-          <option key={monthOption.value} value={monthOption.value}>
-            {monthOption.label}
-          </option>
-        ))}
-      </Select>
-      <Select
-        value={dateSelect.dayValue}
-        onChange={dateSelect.onDayChange}
-        bg="whiteAlpha.900"
-        color="#be5f6f"
-      >
-        <option value="" disabled></option>
-        {dateSelect.dayOptions.map((dayOption) => (
-          <option key={dayOption.value} value={dayOption.value}>
-            {dayOption.label}
-          </option>
-        ))}
-      </Select>
-    </Container>
+    <FormControl isInvalid={date === ""}>
+      <Container>
+        <Select
+          value={dateSelect.yearValue}
+          onChange={dateSelect.onYearChange}
+          bg="whiteAlpha.900"
+          color={dropdownIconColor}
+        >
+          {dateSelect.yearOptions.map((yearOption) => (
+            <option key={yearOption.value} value={yearOption.value}>
+              {yearOption.label}
+            </option>
+          ))}
+        </Select>
+        <Select
+          value={dateSelect.monthValue}
+          onChange={dateSelect.onMonthChange}
+          bg="whiteAlpha.900"
+          color={dropdownIconColor}
+        >
+          {dateSelect.monthOptions.map((monthOption) => (
+            <option key={monthOption.value} value={monthOption.value}>
+              {monthOption.label}
+            </option>
+          ))}
+        </Select>
+        <Select
+          value={dateSelect.dayValue}
+          onChange={dateSelect.onDayChange}
+          bg="whiteAlpha.900"
+          color={dropdownIconColor}
+        >
+          {dateSelect.dayOptions.map((dayOption) => (
+            <option key={dayOption.value} value={dayOption.value}>
+              {dayOption.label}
+            </option>
+          ))}
+        </Select>
+      </Container>
+      <FormErrorMessage color={errorColor}>Invalid date</FormErrorMessage>
+    </FormControl>
   );
 }
 
