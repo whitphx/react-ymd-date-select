0a9de2f4f818e9847421545f41b73c078e2ef72e
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-12 11:52:10 +0900

    Add examples on the website

---
+++ website/samples/options/default-state.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { DateSelect } from "react-ymd-date-select/presets/vanilla";
+
+function Sample() {
+  const [date, setDate] = useState<string>("2022-01-02");
+
+  return (
+    <div>
+      <DateSelect value={date} onChange={setDate} />
+      <p>Selected date is: {date}</p>
+    </div>
+  );
+}
+
+export default Sample;
