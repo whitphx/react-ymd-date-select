0bea629b0be0820f2ae4a7b18df1fcd42e97bf3e
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-13 00:43:22 +0900

    Fix date existence check

--- src/date-string.ts
+++ src/date-string.ts
@@ -1,3 +1,5 @@
+import isDateExists from "date-fns/isExists";
+
 function padZero(value: number, digits: number): string {
   // This implementation is only to be used from `compileDateString()`,
   // assuming the `value` is a positive integer.
@@ -12,24 +14,12 @@ export function compileDateString(
 ): string | null {
   // Create a date string in the format of `yyyy-MM-dd`.
   // For the detailed specs of the format, see https://developer.mozilla.org/en-US/docs/Web/HTML/Date_and_time_formats#date_strings
-  if (month < 1 || 12 < month) {
-    return null;
-  }
-  if (day < 1 || 31 < day) {
+  if (!isDateExists(year, month - 1, day)) {
     return null;
   }
 
   const dateString =
     padZero(year, 4) + "-" + padZero(month, 2) + "-" + padZero(day, 2);
-
-  const dateObj = new Date(dateString);
-  if (
-    dateObj.getFullYear() !== year ||
-    dateObj.getMonth() !== month - 1 ||
-    dateObj.getDate() !== day
-  ) {
-    return null;
-  }
 
   return dateString;
 }
