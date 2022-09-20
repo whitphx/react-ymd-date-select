3224b34fe3b25dd1d300c0018c8c80d20c7bad55
Akira Nitta <rike422+github@gmail.com>
2022-09-20 17:53:35 +0900

    Add workaround for formik type definition errors
    https://github.com/jaredpalmer/formik/issues/3546

--- website/vite-env.d.ts
+++ website/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+// https://github.com/jaredpalmer/formik/issues/3546
+declare namespace React {
+  type StatelessComponent<P> = React.FunctionComponent<P>;
+}
