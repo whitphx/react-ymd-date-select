0cfac97e3d7d8c80934b69c1a52fc7c62d1fb16d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-12-29 10:16:36 +0900

    Remove "type": "module" from package.json for non-ESM module compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,46 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mdx from "@mdx-js/rollup";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    mdx({
-      jsxImportSource: "@emotion/react", // For Emotion. See https://mdxjs.com/docs/getting-started/#emotion
-    }),
-    tsconfigPaths(),
-  ],
-  build: {
-    rollupOptions: {
-      input: [
-        path.resolve(__dirname, "index.html"),
-        path.resolve(__dirname, "/playground/index.html"),
-        path.resolve(__dirname, "/playground/vanilla/index.html"),
-        path.resolve(__dirname, "/playground/mui/index.html"),
-        path.resolve(__dirname, "/playground/chakra-ui/index.html"),
-        path.resolve(
-          __dirname,
-          "/website/components/preset-samples/samples/vanilla/index.html"
-        ),
-        path.resolve(
-          __dirname,
-          "/website/components/preset-samples/samples/chakra-ui/index.html"
-        ),
-        path.resolve(
-          __dirname,
-          "/website/components/preset-samples/samples/mui/index.html"
-        ),
-      ],
+export default defineConfig(async () => {
+  const mdx = (await import("@mdx-js/rollup")).default;
+
+  return {
+    plugins: [
+      react(),
+      mdx({
+        jsxImportSource: "@emotion/react", // For Emotion. See https://mdxjs.com/docs/getting-started/#emotion
+      }),
+      tsconfigPaths(),
+    ],
+    build: {
+      rollupOptions: {
+        input: [
+          path.resolve(__dirname, "index.html"),
+          path.resolve(__dirname, "/playground/index.html"),
+          path.resolve(__dirname, "/playground/vanilla/index.html"),
+          path.resolve(__dirname, "/playground/mui/index.html"),
+          path.resolve(__dirname, "/playground/chakra-ui/index.html"),
+          path.resolve(
+            __dirname,
+            "/website/components/preset-samples/samples/vanilla/index.html"
+          ),
+          path.resolve(
+            __dirname,
+            "/website/components/preset-samples/samples/chakra-ui/index.html"
+          ),
+          path.resolve(
+            __dirname,
+            "/website/components/preset-samples/samples/mui/index.html"
+          ),
+        ],
+      },
     },
-  },
-  test: {
-    environment: "happy-dom", // https://vitest.dev/guide/features.html#mocking
-    setupFiles: "./setupTest.ts",
-  },
+    test: {
+      environment: "happy-dom", // https://vitest.dev/guide/features.html#mocking
+      setupFiles: "./setupTest.ts",
+    },
+  };
 });
